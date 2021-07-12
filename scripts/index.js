8ea6f4e598c52215f8982866cc8925a09568bf6b
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2021-07-11 17:26:27 -1000

    WIP: theme design

---
+++ scripts/index.js
@@ -0,0 +1,57 @@
+const project_array = [
+  {
+    project_name: "Parks and Recreation Online",
+    href: "https://pros.hnl.info",
+    project_image: "./assets/images/pros.png",
+  },
+  {
+    project_name: "Parks and Recreation Online",
+    href: "https://pros.hnl.info",
+    project_image: "./assets/images/pros.png",
+  },
+  {
+    project_name: "Parks and Recreation Online",
+    href: "https://pros.hnl.info",
+    project_image: "./assets/images/pros.png",
+  },
+  {
+    project_name: "Parks and Recreation Online",
+    href: "https://pros.hnl.info",
+    project_image: "./assets/images/pros.png",
+  },
+  {
+    project_name: "Parks and Recreation Online",
+    href: "https://pros.hnl.info",
+    project_image: "./assets/images/pros.png",
+  },
+  {
+    project_name: "Parks and Recreation Online",
+    href: "https://pros.hnl.info",
+    project_image: "./assets/images/pros.png",
+  },
+];
+
+(function () {
+  const project_list = document.querySelector("#project-links");
+  project_array.forEach((project) => {
+    const project_list_item = document.createElement("li");
+    const image_container = document.createElement("a");
+    const image = document.createElement("img");
+    const link_item_text = document.createElement("a");
+
+    image.src = project.project_image;
+    image.className = "image";
+    image_container.className = "image-container";
+    image_container.style.backgroundImage = project.project_image;
+    project_list_item.className = "link-list-container";
+
+    image_container.href = project.href;
+    link_item_text.className = "link";
+    link_item_text.textContent = project.project_name;
+
+    image_container.append(image);
+    image_container.append(link_item_text);
+    project_list_item.append(image_container);
+    project_list.append(project_list_item);
+  });
+})();
